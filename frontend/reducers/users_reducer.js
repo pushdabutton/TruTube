abffa5617b12473540fbd09fd482e6ca5c3a0e99
Alex Seant <ipushdabutton@gmail.com>
2019-05-30 09:20:44 -0400

    5th commit

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -5,7 +5,6 @@ import { RECEIVE_CURRENT_USER } from "../actions/session_actions"
 
 const usersReducer = (oldState = {}, action) => {
     Object.freeze(oldState)
-    debugger
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
             return merge({}, oldState, {[action.user.id]: action.user})
