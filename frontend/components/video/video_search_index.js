6f40d6e2427a5f5506fe926e8b8167fdbdeee430
Alex Seant <ipushdabutton@gmail.com>
2019-06-27 15:36:15 -0400

    last minute dark mode fix

--- frontend/components/video/video_search_index.js
+++ frontend/components/video/video_search_index.js
@@ -5,6 +5,11 @@ import Randomize from '../../selectors/random_selector'
 
 class VideoSearchIndex extends React.Component {
 
+    constructor(props){
+        super(props)
+        this.redirect = this.redirect.bind(this)
+    }
+
     componentDidMount() {
         let query = this.props.history.location.search.slice(7)
         let params = { query }
@@ -19,25 +24,36 @@ class VideoSearchIndex extends React.Component {
         }
     }
 
+    redirect(id) {
+        this.props.history.push(`/videos/${id}`)
+    }
+
     render() {
+        let mode = this.props.ui.darkmode ? 'dark' : 'light'
 
         if (!this.props.videos) { return null }
 
         let videos = this.props.videos.map(video => {
             return (
+                <div className={`${mode}-vid-holder`} onClick={() => this.redirect(video.id)}> 
                 <VideoIndexItem key={video.id} video={video} ui={this.props.ui} />
+                <p>{video.title}</p>
+                <div className="mini-video-user">{video.username}</div>
+                <div className="mini-video-info"><span className="mini-views">{video.views || 0} views</span>&nbsp;•&nbsp;<span className="mini-views">{video.published}</span></div>
+                </div>
             );
         });
-        let mode = this.props.ui.darkmode ? 'dark' : 'light'
+        
 
         return (
             <div className={`${mode}-frame`}>
-                {leftNav()}
+                {leftNav(mode)}
                 <div className="video-index-list">
-                    <p className="Catergory-Words">Search Results</p>
+                    <p className={`${mode}-Catergory-Words`}>Search Results</p>
                     <div className="index-video-box">
                         {videos}
                     </div>
+                    
                 </div>
 
             </div>
