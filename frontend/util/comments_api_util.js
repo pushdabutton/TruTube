06f2786252d1053d5a33f56575769fe394b7aab7
Alex Seant <ipushdabutton@gmail.com>
2020-01-06 20:00:00 -0600

    video update 4

--- frontend/util/comments_api_util.js
+++ frontend/util/comments_api_util.js
@@ -11,7 +11,7 @@ export const fetchComments = id => {
 
 export const createComment = comment => (
     $.ajax({
-        
+
         url: `api/videos/${comment.author_id}/comments`,
         method: 'POST',
         data: { comment }
@@ -20,6 +20,7 @@ export const createComment = comment => (
 
 export const deleteComment = comment => (
     $.ajax({
+        
         url: `api/videos/${comment.video_id}/comments/${comment.id}`,
         method: 'DELETE'
     })
